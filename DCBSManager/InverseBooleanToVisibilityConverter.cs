4e189c370a1b341f4ea9cf9132186f78a792d02d
Brian <Brian@Smilodon>
2014-06-05 11:39:21 -0700

    ADDED: InverseBooleanToVisibilityConverter added to project

---
+++ DCBSManager/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+
+
+namespace DCBSManager
+{
+	public class InverseBooleanToVisibilityConverter : IValueConverter {
+		#region IValueConverter Members
+
+		public object Convert(object value, Type targetType, object parameter,
+				System.Globalization.CultureInfo culture) {
+					if ( (value is bool) == false) {
+						throw new InvalidOperationException("The target must be a boolean");
+					}
+
+					bool show = (bool)value;
+			return ( (!show) ? Visibility.Visible : Visibility.Collapsed );
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter,
+				System.Globalization.CultureInfo culture) {
+					if (value is Visibility) {
+						throw new InvalidOperationException("The target must be a Visiblity state");
+					}
+
+					return !(((Visibility)value).Equals(Visibility.Visible));
+		}
+
+		#endregion
+	}
+}
